7b2ff4ecdd5e4815b19e79854eac69718bf687be
Benandry <nandry556@gmail.com>
2022-12-30 12:19:47 +0100

    Debug

--- assets/api/remboursement.js
+++ assets/api/remboursement.js
@@ -3,24 +3,24 @@ import $ from 'jquery'
 $(document).ready(function(){
     var recupe_codecredit=$('#codecreditremboursement').text()
     // var codecredit=$('#remboursement_codecredit').val(recupe_codecredit)
-    console.log(recupe_codecredit)
+
     
     var url_api='/remboursement_credit/'+recupe_codecredit;
 
-    $('#remboursement_dateRemborsement').on('blur',function(){
+    console.log(url_api);
         $.ajax({
-            url:url_api,
-            method:'GET',
-            dataType:"json",
-            contentType:"application/json; charset=utf-8",
-            success :function(data){
-                for(let j=0;j<data.length;j++){
-                    var clientcredit=data[j];
-                    console.log(clientcredit);
-                }
+            url: url_api,
+            method: "GET",
+            dataType : "json",
+            contentType: "application/json; charset=utf-8",
+            data : JSON.stringify(),
+            success: function(result){
 
+                console.log(result);
+                for (let i = 0; i < result.length; i++) {
+                    var element = result[i];
+                
+                }
             }
         });
-    })
-
     })
